a9c49831bbe329c129609c39b9f30f179fbfbfa8
tommchenry <tommchenry@gmail.com>
2016-02-07 20:31:45 -0600

    Add initial MVP.

---
+++ db/seeds.rb
@@ -0,0 +1,12 @@
+# This file should contain all the record creation needed to seed the database with its default values.
+# The data can then be loaded with the rake db:seed (or created alongside the db with db:setup).
+#
+# Examples:
+#
+#   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
+#   Mayor.create(name: 'Emanuel', city: cities.first)
+require 'faker'
+
+10.times do
+  Article.create(title: Faker::Hacker.noun, body: Faker::Hacker.say_something_smart, published?: true )
+end
